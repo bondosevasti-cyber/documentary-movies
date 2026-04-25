1d08c59079f04f9fff9d751859bdac0b57926ce3
bondosevasti-cyber <bondosevasti@gmail.com>
2026-04-25 20:05:17 +0400

    refactor: standardize project structure and dependencies for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    transpilePackages: ['@tiptap/extension-image', '@tiptap/react', '@tiptap/pm', '@tiptap/core']
 };
 
 export default nextConfig;
